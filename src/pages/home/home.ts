7eb0376b972007f218a21070bbcbd3f76d29160b
NEXON\ki502 <ki502@nexon.co.kr>
2018-03-09 18:31:29 +0900

    1. typescript duplication valuable ok 2. ground, background image add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Content } from 'ionic-angular';
+import { Content, Platform } from 'ionic-angular';
 import * as PIXI from 'pixi.js';
 import "pixi-layers";
 
@@ -10,31 +10,77 @@ import "pixi-layers";
 export class HomePage {
   @ViewChild(Content) content : Content;
 
-  constructor() {
+  constructor(public platform: Platform) {
   }
 
   ngAfterViewInit(){
-    var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb, autoStart: true});
+    const WIDTH = this.platform.width();
+    const HEIGHT = this.platform.height();
+
+    var app = new PIXI.Application(WIDTH, HEIGHT, {backgroundColor : 0x1099bb, autoStart: true});
     this.content.getNativeElement().appendChild(app.view);
+
     var container = new PIXI.Container();
+
+    var backgroundGroup = new PIXI.display.Group(0, true);
+    var backgroundLayer = new PIXI.display.Layer(backgroundGroup);
+    container.parentGroup = backgroundGroup;
+    backgroundLayer.group.enableSort = true;
+    container.addChild(backgroundLayer);
+    
+    //1280x800
+    var backgroundTexture = PIXI.Texture.fromImage("assets/imgs/bg.png");
+    var backgroundSprite = new PIXI.Sprite(backgroundTexture);
+    backgroundSprite.width = WIDTH;
+    backgroundSprite.height = HEIGHT;
+    container.addChild(backgroundSprite);
+
+    //900x1090
+    var bambooContainer = new PIXI.Container();
+    bambooContainer.parentGroup = backgroundGroup;
+    var bambooCount = (WIDTH/900) <= 0 ? 1 : (WIDTH/900);
+
+    for(var index = 0; index < bambooCount + 1; index++) {
+      var texture = PIXI.Texture.fromImage("assets/imgs/bamboo.png");
+      var sprite = new PIXI.Sprite(texture);
+      sprite.on("update" ,function(data){
+        this.position.x = (this.position.x + this.width - data.delta) < 0 ? WIDTH : this.position.x - data.delta;
+      });
+      sprite.position.x = (WIDTH / bambooCount) * index;
+      sprite.width = WIDTH / bambooCount;
+      sprite.height = HEIGHT;
+      bambooContainer.addChild(sprite);
+    }
+
+    container.addChild(bambooContainer);
+
+    var objectGroup = new PIXI.display.Group(1, true);
+    var objectLayer = new PIXI.display.Layer(objectGroup);
+    container.addChild(objectLayer);
+
+    var groundContainer = new PIXI.Container();
+    groundContainer.parentGroup = objectGroup;
+    var groundCount = (WIDTH/220) <= 0 ? 1 : (WIDTH/220);
+
+    for(var index = 0; index < groundCount + 1; index++) {
+      var texture = PIXI.Texture.fromImage("assets/imgs/ground.png");
+      var sprite = new PIXI.Sprite(texture);
+      sprite.position.x = (WIDTH / groundCount) * index;
+      sprite.position.y = HEIGHT - 90;
+      sprite.width = WIDTH / groundCount;
+      groundContainer.addChild(sprite);
+    }
+    
+    container.addChild(groundContainer);
     app.stage.addChild(container);
 
-    var group = new PIXI.display.Group(0, true);
-    app.stage.addChild(new PIXI.display.Layer(group));
-    
-    var bg = new PIXI.Sprite(PIXI.RenderTexture.fromImage('assets/imgs/bg.png'));
-    bg.parentGroup = group;
-
-    container.addChild(bg);   
-
-    // // Listen for animate update
-    // app.ticker.add(function(delta) {
-    //     // just for fun, let's rotate mr rabbit a little
-    //     // delta is 1 if running at 100% performance
-    //     // creates frame-independent transformation
-    //     bunny.rotation += 0.1 * delta;
-    // });
-
+    app.ticker.add(function(delta) {
+      bambooContainer.children.forEach(function(displayObject, index){
+        displayObject.emit("update", {
+          delta: delta
+        });
+      });
+    });
   }
 
   
